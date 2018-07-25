ed7bc038f8fe26d9b8cdd49ba7f2cfb94f879b60
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-07-25 15:45:19 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  famous_animals = ["lions", "tigers", "bears"]
+ 
+famous_cats << "zibra"
 end
 
 def add_element_to_start_of_array(array, element)
